1bc024cc071916c765de0d31c520e475c219ef5a
Leo-Volkov <Leo.Volkov156@yandex.ru>
2024-04-04 16:48:35 +0300

    .

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
-
-console.log( navigator.userAgent.toLowerCase())
+let v_rend = document.querySelector(`.v_rend`);
+v_rend.innerHTML = navigator.userAgent;
 // ......................
 
+let elemet = document.querySelector(`div`);
+let s_rend = document.querySelector(`.s_rend`);
+let h3 = document.querySelector(`h3`);
 
-// let elemet = document.querySelector(`div`);
-// let rend = document.querySelector(`.rend`);
+document
+    .querySelector(`.button_start`)
+    .addEventListener("click", function (evt) {
+        let i = 0;
+        let p = ``;
+        for (const index in elemet.style) {
+            p += `<li>${index}</li>`;
+            i++;
+        }
+        h3.innerHTML = `В браузере доступно ${i} ${getEnding(i)}`;
+        s_rend.innerHTML = p;
 
-// let h1 = document.querySelector(`h1`);
-// const userAgent = navigator.userAgent;
-// const browserName = userAgent.match(/([^\s]+)\/.+/)[1];
-// h1.innerHTML = `Проверить свойств поддерживает (${browserName}) браузер`
+        // console.log(`Успешнно всё работает, Молодец👍`);
+    });
 
-// document
-//   .querySelector(`.button_start`)
-//   .addEventListener("click", function (evt) {
+document.querySelector(`.cut`).addEventListener("click", function (evt) {
+    let input = document.querySelector(".s_rend");
+    navigator.clipboard.writeText(navigator.userAgent).catch((err) => {
+        console.log("Скопировать в буфер не удалось", err);
+    });
+});
 
-//     let i = 0
-//     let p = `` 
-//     for (const index in elemet.style) {
-//       p += `
-// ${index}`
-//       i++
-//     };
-//     console.log(i)
-//     rend.innerHTML = `${i}` + p
-    
-//     // console.log(`Успешнно всё работает, Молодец👍`);
-//   });
 
-//   function cut() {
-//     let input = document.querySelector('.rend');
-//     navigator.clipboard
-//       .writeText(input.innerHTML)
-//       .catch((err) => {
-//         console.log("Скопировать в буфер не удалось", err);
-//       });
-//   }
+function getEnding(i) {
+    let ending;
+    const lastDigit = i % 10;
+  
+    switch (lastDigit) {
+      case 1:
+        ending = "свойство";
+        break;
+      case 2:
+      case 3:
+      case 4:
+        ending = "свойтва";
+        break;
+      default:
+        ending = "свойствa";
+    }
+  
+    return ending;
+  }
